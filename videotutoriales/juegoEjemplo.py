1f3a82381e1f28a1e3213fb11290b21ff0d34650
Joel Torres <joelsebastiantc@gmail.com>
2020-07-17 18:36:55 -0400

    Modificacion de juego, nueva carpeta para video tutoriales

--- videotutoriales/juegoEjemplo.py
+++ videotutoriales/juegoEjemplo.py
@@ -9,8 +9,8 @@ pygame.display.set_caption("MI PRIMER JUEGO")
 reloj = pygame.time.Clock()
 
 #incluir en clase proyectil
-sonidoBala = pygame.mixer.Sound('snd/bullet.wav')
-sonidoGolpe = pygame.mixer.Sound('snd/hit.wav')
+sonido_bala = pygame.mixer.Sound('snd/bullet.wav')
+sonido_golpe = pygame.mixer.Sound('snd/hit.wav')
 
 # es necesario un standar de carga
 class personaje(object):
@@ -21,82 +21,69 @@ class personaje(object):
 		self.ancho = ancho
 		self.alto = alto
 		self.velocidad = 5
-		self.haSaltado = False
-		self.impulsoSalto = 10
+		self.ha_saltado = False
+		self.impulso_salto = 10
 		self.izquierda = False
 		self.derecha = False
-		self.contadorPasos = 0
+		self.contador_pasos = 0
 		self.fin = fin
 		self.camino = [self.x, self.fin]
-		self.zonaImpacto = (self.x + 15, self.y + 2, 30, 55)
+		self.zona_impacto = (self.x + 15, self.y + 2, 30, 55)
 		self.salud = 10
-		self.esVisible = True
+		self.es_visible = True
 		fuente += "/"
-		self.caminaIzquierda = [pygame.image.load("img/"+fuente+"L1.png"),pygame.image.load("img/"+fuente+"L2.png"),pygame.image.load("img/"+fuente+"L3.png"),pygame.image.load("img/"+fuente+"L4.png"),pygame.image.load("img/"+fuente+"L5.png"),pygame.image.load("img/"+fuente+"L6.png"),pygame.image.load("img/"+fuente+"L7.png"),pygame.image.load("img/"+fuente+"L8.png"),pygame.image.load("img/"+fuente+"L9.png")]
-		self.caminaDerecha = [pygame.image.load("img/"+fuente+"R1.png"),pygame.image.load("img/"+fuente+"R2.png"),pygame.image.load("img/"+fuente+"R3.png"),pygame.image.load("img/"+fuente+"R4.png"),pygame.image.load("img/"+fuente+"R5.png"),pygame.image.load("img/"+fuente+"R6.png"),pygame.image.load("img/"+fuente+"R7.png"),pygame.image.load("img/"+fuente+"R8.png"),pygame.image.load("img/"+fuente+"R9.png")]
+		self.camina_izquierda = [pygame.image.load("img/"+fuente+"L1.png"),pygame.image.load("img/"+fuente+"L2.png"),pygame.image.load("img/"+fuente+"L3.png"),pygame.image.load("img/"+fuente+"L4.png"),pygame.image.load("img/"+fuente+"L5.png"),pygame.image.load("img/"+fuente+"L6.png"),pygame.image.load("img/"+fuente+"L7.png"),pygame.image.load("img/"+fuente+"L8.png"),pygame.image.load("img/"+fuente+"L9.png")]
+		self.camina_derecha = [pygame.image.load("img/"+fuente+"R1.png"),pygame.image.load("img/"+fuente+"R2.png"),pygame.image.load("img/"+fuente+"R3.png"),pygame.image.load("img/"+fuente+"R4.png"),pygame.image.load("img/"+fuente+"R5.png"),pygame.image.load("img/"+fuente+"R6.png"),pygame.image.load("img/"+fuente+"R7.png"),pygame.image.load("img/"+fuente+"R8.png"),pygame.image.load("img/"+fuente+"R9.png")]
 		self.quieto = pygame.image.load("img/"+fuente+"standing.png")
 
 	def dibujar(self, cuadro):
-		if self.contadorPasos + 1 > 27:
-			self.contadorPasos = 0
+		if self.contador_pasos + 1 > 27:
+			self.contador_pasos = 0
 
-		if self.esVisible:
+		if self.es_visible:
 			if self.izquierda:
-				cuadro.blit(self.caminaIzquierda[self.contadorPasos//3],(self.x,self.y))
-				self.contadorPasos += 1
+				cuadro.blit(self.camina_izquierda[self.contador_pasos//3],(self.x,self.y))
+				self.contador_pasos += 1
 			elif self.derecha:
-				cuadro.blit(self.caminaDerecha[self.contadorPasos//3],(self.x,self.y))
-				self.contadorPasos += 1
+				cuadro.blit(self.camina_derecha[self.contador_pasos//3],(self.x,self.y))
+				self.contador_pasos += 1
 			else:
 				if self.derecha:
-					cuadro.blit(self.caminaDerecha[0],(self.x,self.y))
+					cuadro.blit(self.camina_derecha[0],(self.x,self.y))
 				elif self.izquierda:
-					cuadro.blit(self.caminaIzquierda[0],(self.x,self.y))
+					cuadro.blit(self.camina_izquierda[0],(self.x,self.y))
 				else:
 					cuadro.blit(self.quieto, (self.x,self.y))
-			self.zonaImpacto = (self.x + 15, self.y + 10, 30, 50)
+			self.zona_impacto = (self.x + 15, self.y + 10, 30, 50)
 			#Crear clase barra de vida
-			pygame.draw.rect(cuadro, (255,0,0), (self.zonaImpacto[0], self.zonaImpacto[1] - 20, 50, 10))
-			pygame.draw.rect(cuadro, (0,128,0), (self.zonaImpacto[0], self.zonaImpacto[1] - 20, 50 - (5 * (10 - self.salud)), 10))
+			pygame.draw.rect(cuadro, (255,0,0), (self.zona_impacto[0], self.zona_impacto[1] - 20, 50, 10))
+			pygame.draw.rect(cuadro, (0,128,0), (self.zona_impacto[0], self.zona_impacto[1] - 20, 50 - (5 * (10 - self.salud)), 10))
 			#En caso de querer visualizar el hitbox, descomentar la siguiente linea
-			#pygame.draw.rect(cuadro, (255,0,0), self.zonaImpacto, 2)
+			#pygame.draw.rect(cuadro, (255,0,0), self.zona_impacto, 2)
 		else:
-			if self.zonaImpacto[0] != -1:
+			if self.zona_impacto[0] != -1:
 				texto = pygame.font.SysFont('comicsans',100)
 				marcador = texto.render('GANASTE!', 1, (255,0,0))
 				cuadro.blit(marcador, (250 - (marcador.get_width()/2),200))
 				pygame.display.update()
 				pygame.time.delay(2000)
-			self.zonaImpacto = (-1, -1, -1, -1)
+			self.zona_impacto = (-1, -1, -1, -1)
 
-
-	def impactarFinal(self,cuadro):
-		self.haSaltado = False
-		self.impulsoSalto = 10
+	def impactar_final(self,cuadro):
+		self.ha_saltado = False
+		self.impulso_salto = 10
 		self.x = 100
 		self.y = 410
-		self.contadorPasos = 0
-		'''
-		texto = pygame.font.SysFont('comicsans',100)
-		marcador = texto.render('-5', 1, (255,0,0))
-		cuadro.blit(marcador, (250 - (marcador.get_width()/2),200))
-		pygame.display.update()
-		'''
+		self.contador_pasos = 0
 		pygame.time.delay(2000)
 
 	def impactar(self, cuadro):
 		if self.salud > 0:
 			self.salud -= 1
 		else:
-			self.esVisible = False
+			self.es_visible = False
 			del(self)
 
-		'''
-		texto = pygame.font.SysFont('comicsans',100)
-		marcador = texto.render('-1', 1, (255,0,0))
-		cuadro.blit(marcador, (250 - (marcador.get_width()/2),200))
-		pygame.display.update()
-		'''
 	def capturar_movimiento(self, k, iz, de, ar, ab, salta):
 		if k[iz] and self.x > self.velocidad:
 			self.x -= self.velocidad
@@ -110,10 +97,10 @@ class personaje(object):
 		else:
 			self.izquierda = False
 			self.derecha = False
-			self.contadorPasos = 0
+			self.contador_pasos = 0
 
 
-		if not self.haSaltado:
+		if not self.ha_saltado:
 			if k[ar] and self.y > self.velocidad:
 				self.y -= self.velocidad
 
@@ -121,22 +108,22 @@ class personaje(object):
 				self.y += self.velocidad
 
 			if k[salta]:
-				self.haSaltado = True
+				self.ha_saltado = True
 				self.izquierda = False
 				self.derecha = False
-				self.contadorPasos = 0
+				self.contador_pasos = 0
 		else:
-			if self.impulsoSalto >= -10:
-				if self.impulsoSalto < 0:
-					self.y -= (self.impulsoSalto**2) * 0.5 * -1
+			if self.impulso_salto >= -10:
+				if self.impulso_salto < 0:
+					self.y -= (self.impulso_salto**2) * 0.5 * -1
 				else:
-					self.y -= (self.impulsoSalto**2) * 0.5
-				self.impulsoSalto -= 1
+					self.y -= (self.impulso_salto**2) * 0.5
+				self.impulso_salto -= 1
 			else:
-				self.haSaltado = False
-				self.impulsoSalto = 10
+				self.ha_saltado = False
+				self.impulso_salto = 10
 
-	def mover_solo(self,cuadro, nivel):
+	def mover_solo(self, cuadro, nivel):
 		if self.velocidad > 0:
 			if self.x + self.velocidad < self.camino[1]:
 				self.x += self.velocidad * nivel 
@@ -144,7 +131,7 @@ class personaje(object):
 				self.izquierda = False
 			else:
 				self.velocidad = self.velocidad * -1
-				self.contadorPasos = 0
+				self.contador_pasos = 0
 		else:
 			if self.x - self.velocidad > self.camino[0]:
 				self.x += self.velocidad * nivel 
@@ -152,17 +139,17 @@ class personaje(object):
 				self.derecha = False
 			else:
 				self.velocidad = self.velocidad * -1
-				self.contadorPasos = 0
+				self.contador_pasos = 0
 
-	def meHaTocao(self, tocador):
-		R1_ab=self.zonaImpacto[1] + self.zonaImpacto[3]
-		R1_ar=self.zonaImpacto[1]
-		R1_iz=self.zonaImpacto[0]
-		R1_de=self.zonaImpacto[0] + self.zonaImpacto[2]
-		R2_ab=tocador.zonaImpacto[1] + tocador.zonaImpacto[3]
-		R2_ar=tocador.zonaImpacto[1]
-		R2_iz=tocador.zonaImpacto[0]
-		R2_de=tocador.zonaImpacto[0] + tocador.zonaImpacto[2]
+	def me_ha_tocao(self, tocador):
+		R1_ab=self.zona_impacto[1] + self.zona_impacto[3]
+		R1_ar=self.zona_impacto[1]
+		R1_iz=self.zona_impacto[0]
+		R1_de=self.zona_impacto[0] + self.zona_impacto[2]
+		R2_ab=tocador.zona_impacto[1] + tocador.zona_impacto[3]
+		R2_ar=tocador.zona_impacto[1]
+		R2_iz=tocador.zona_impacto[0]
+		R2_de=tocador.zona_impacto[0] + tocador.zona_impacto[2]
 
 		return R1_de > R2_iz and R1_iz < R2_de and R1_ar < R2_ab and R1_ab > R2_ar
 
@@ -177,15 +164,15 @@ class proyectil(object):
 		self.color = color
 		self.direccion = direccion
 		self.velocidad = 8 * direccion
-		self.zonaImpacto = (self.x-self.radio, self.y-self.radio, self.radio*2, self.radio*2)
+		self.zona_impacto = (self.x-self.radio, self.y-self.radio, self.radio*2, self.radio*2)
 
 	def dibujar(self, cuadro):
-		self.zonaImpacto = (self.x-self.radio, self.y-self.radio, self.radio*2, self.radio*2)
+		self.zona_impacto = (self.x-self.radio, self.y-self.radio, self.radio*2, self.radio*2)
 		pygame.draw.circle(cuadro, self.color, (self.x, self.y), self.radio)
 		#En caso de querer visualizar el hitbos, descomentar la siguiente linea
-		#pygame.draw.rect(cuadro, (255,0,0), self.zonaImpacto, 2)
+		#pygame.draw.rect(cuadro, (255,0,0), self.zona_impacto, 2)
 
-def rePintarCuadroJuego():
+def repintar_cuadro_juego():
 	#Dibujar fondo del nivel
 	if nivel <= nivel_maximo:
 		ventana.blit(imagen_fondo[nivel],(0,0))
@@ -205,7 +192,7 @@ def rePintarCuadroJuego():
 		bala.dibujar(ventana)
 	pygame.display.update()
 
-def subirNivel():
+def subir_nivel():
 	global nivel
 	global nivel_maximo
 	global villano
@@ -284,7 +271,7 @@ while repetir:
 
 
 		mono.dibujar(ventana)
-		pygame.display.update();
+		pygame.display.update()
 
 	while run:
 		reloj.tick(27)
@@ -294,9 +281,9 @@ while repetir:
 				quit()
 
 		#contacto directo con villano
-		if villano.esVisible:
-			if heroe.meHaTocao(villano):
-				heroe.impactarFinal(ventana)
+		if villano.es_visible:
+			if heroe.me_ha_tocao(villano):
+				heroe.impactar_final(ventana)
 				puntaje -= 5
 				heroe.salud -= 5
 
@@ -307,8 +294,8 @@ while repetir:
 
 		#contacto de proyectil con el villano
 		for bala in balas:
-			if villano.meHaTocao(bala):
-				sonidoGolpe.play()
+			if villano.me_ha_tocao(bala):
+				sonido_golpe.play()
 				villano.impactar(ventana)
 				puntaje += 1
 				balas.pop(balas.index(bala))
@@ -332,7 +319,7 @@ while repetir:
 
 			if len(balas) < 5: # balas en pantalla
 				balas.append(proyectil(round(heroe.x + heroe.ancho // 2), round(heroe.y + heroe.alto // 2), 6, (0,0,0), direccion))
-				sonidoBala.play()
+				sonido_bala.play()
 			tanda_disparos = 1
 
 
@@ -343,11 +330,11 @@ while repetir:
 
 		# Consulta para saber si se sube de nivel
 		if villano.salud <= 0:
-			subirNivel()
+			subir_nivel()
 		if heroe.salud <= 0:
 			run = False
 
-		rePintarCuadroJuego()
+		repintar_cuadro_juego()
 		
 	final = True
 	while final:
